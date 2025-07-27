749ff1a61e899e1b2fb8a7e05146a2627fff3a1d
JSchmerling <mail@schmerl.ing>
2025-07-27 20:03:33 +0200

    Update ExtendedView.java

--- app/src/main/java/net/gsantner/markor/frontend/ExtendedView.java
+++ app/src/main/java/net/gsantner/markor/frontend/ExtendedView.java
@@ -1,1 +1,13 @@
+import android.view.Menu;
+import android.view.MenuItem;
 
+public class MenuUtils {
+    public static void hideActionsMenu(Menu menu) {
+        if (menu != null) {
+            MenuItem actions = menu.findItem(R.id.actions);
+            if (actions != null) {
+                actions.setVisible(false);
+            }
+        }
+    }
+}
