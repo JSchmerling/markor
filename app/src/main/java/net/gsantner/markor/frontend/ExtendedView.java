67319e7c64cff48bcbb4fa32de080bf8c69c8eac
JSchmerling <mail@schmerl.ing>
2025-07-28 17:14:53 +0200

    Update ExtendedView.java

--- app/src/main/java/net/gsantner/markor/frontend/ExtendedView.java
+++ app/src/main/java/net/gsantner/markor/frontend/ExtendedView.java
@@ -3,6 +3,7 @@ import net.gsantner.markor.R;
 
 public class ExtendedView {
     public static void hideMenu(Menu menu) {
-        menu.setGroupVisible(R.id.actions, false);
+        menu.setGroupVisible(R.id.document__edit__menu, false);
+        menu.setGroupVisible(R.id.main__bottom__nav, false);
     }
 }
